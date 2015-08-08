025b32334d7628331e38227dc71787b8eca5c880
guomin.li <guomin.li@dmall.com>
2015-08-08 17:46:40 +0800

    Initial Commit

---
+++ swift-setterAndGetter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-setterAndGetter
+//
+//  Created by 李国民 on 15/8/8.
+//  Copyright (c) 2015年 李国民. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
